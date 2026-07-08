e165cc62f241db9c8feb1c9cba007fc620aa0e2f
Raul Catalan <raulrcatalan@gmail.com>
2026-07-08 06:52:44 -0700

    Configurate Zeus Monitors

--- home/hypr/monitors.nix
+++ home/hypr/monitors.nix
@@ -14,15 +14,25 @@
       */
       monitor = [
         {
+          # VGA245
           output = "HDMI-A-2";
           mode = "1920x1080@60";
           position = "0x0";
           scale = "1.0";
+          transform = 1;
         }
         {
+          # VGA32AQL1A
           output = "HDMI-A-1";
           mode = "2560x1440@144";
           position = "1080x0";
+          scale = "1.0";
+        }
+        {
+          # Fallback/default
+          output = "";
+          mode = "preferred";
+          position = "auto";
           scale = "1.0";
         }
       ];
