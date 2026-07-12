3a55af8fcb5e77ef6f3e6f50e9e9f3c9bc1b4b38
Raul Catalan <raulrcatalan@gmail.com>
2026-07-11 18:13:19 -0700

    Add Window Rules to Steam

--- modules/home/hypr/rules.nix
+++ modules/home/hypr/rules.nix
@@ -1,11 +1,21 @@
 {
   wayland.windowManager.hyprland.settings = {
-    window_rule = {
-      match = {
-        class = "^(kitty)$";
-      };
-      float = true;
-      border_size = 10;
-    };
+    window_rule = [
+      {
+        # Testing Rule
+        match = {
+          class = "^(kitty)$";
+        };
+        float = true;
+        border_size = 10;
+      }
+      {
+        match = {
+          class = "^(steam)$";
+          title = "negative:^(Steam)$";
+        };
+        float = true;
+      }
+    ];
   };
 }
